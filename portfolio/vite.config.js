ec739317a05371ab172b51562dcee0c4b883df83
FelixAlexander1 <alemongar022@gmail.com>
2025-04-09 21:44:06 +0100

    fix: index vol.5

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',  // Puedes cambiar esta carpeta si lo deseas
+    outDir: 'dist', // Define la carpeta de salida
+    rollupOptions: {
+      input: 'index.html', // Asegúrate de que este archivo está presente
+    },
   },
 });
